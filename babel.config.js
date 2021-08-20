c1987b79325f488949de7ba293b8878bfeb3afc1
Farid Hernandez <farid.hdz.64@gmail.com>
2021-08-19 23:58:53 -0600

    feat: Adding basic configuration for typescript + eslint + prettier

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,43 @@
-module.exports = function(api) {
+const path = require('path');
+const pak = require('../sds-react-native-components/package.json');
+// const pak = {
+//   name: '@2600hz/sds-react-native-components',
+//   source: 'src/index',
+// };
+
+// sds-react-native-components
+const dirName = pak.name.split('/')[1];
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src',
+            '@containers': './src/containers',
+            '@components': './src/components',
+            '@modules': './src/modules',
+            '@styles': './src/styles',
+            tests: ['./tests/'],
+
+            // For development, we want to alias the library to the source
+            [pak.name]: path.join(__dirname, '..', dirName, pak.source),
+          },
+        },
+      ],
+      // [
+      //   'babel-plugin-inline-import',
+      //   {
+      //     extensions: ['.svg'],
+      //   },
+      // ],
+    ],
   };
 };
